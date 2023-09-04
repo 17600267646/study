e978962e3add3b86882c94db4f7e7b227d0604d3
17600267646 <17600267646@163.com>
2023-09-04 18:37:24 +0800

    优化结构

--- src/main/java/com/person/study/controller/RedisController.java
+++ src/main/java/com/person/study/controller/RedisController.java
@@ -3,19 +3,14 @@ package com.person.study.controller;
 
 import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.RandomUtil;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.person.study.code.ApiResponse;
-import com.person.study.entity.MyjkUserdataUserFootRecord;
 import com.person.study.entity.model.MyRedisModel;
 import com.person.study.service.MyjkUserdataUserFootRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,20 +23,15 @@ import java.util.Set;
  * @since 2023-09-03
  */
 @RestController
-@RequestMapping("/myjk")
-public class MyjkUserdataUserFootRecordController {
+@RequestMapping("/redis")
+public class RedisController {
 
     @Autowired
     private MyjkUserdataUserFootRecordService myjkUserdataUserFootRecordService;
     @Autowired
     private RedisTemplate redisTemplate;
 
-    @GetMapping("/hello")
-    public ApiResponse hello() {
-        List<MyjkUserdataUserFootRecord> list = myjkUserdataUserFootRecordService.list(new QueryWrapper<>());
 
-        return ApiResponse.builder().code(200).message("success").data(list).build();
-    }
 
     @GetMapping("/myredis")
     public MyRedisModel myredis() {
