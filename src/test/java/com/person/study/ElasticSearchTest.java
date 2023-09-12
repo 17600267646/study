8db1b7905d11d8a25afd118c386120fea1d611e4
17600267646 <17600267646@163.com>
2023-09-12 17:49:53 +0800

    es查询

--- src/test/java/com/person/study/ElasticSearchTest.java
+++ src/test/java/com/person/study/ElasticSearchTest.java
@@ -1,6 +1,9 @@
 package com.person.study;
 
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.json.JSONUtil;
 import com.person.study.config.ElasticSearchConfig;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.DocWriteResponse;
@@ -8,16 +11,21 @@ import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.*;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +35,7 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
@@ -45,12 +54,13 @@ public class ElasticSearchTest {
 
     /**
      * 创建索引 插入文档
+     *
      * @throws IOException
      */
     @Test
     public void test001() throws IOException {
         IndexRequest indexRequest = new IndexRequest("indexrequest_1");
-        indexRequest.id(UUID.randomUUID().toString().replaceAll("-",""));
+        indexRequest.id(UUID.randomUUID().toString().replaceAll("-", ""));
         String jsonString = "{" +
                 "\"user\":\"kimchy\"," +
                 "\"postDate\":\"2013-01-30\"," +
@@ -61,13 +71,14 @@ public class ElasticSearchTest {
         indexRequest.type(XContentType.JSON.mediaType());
         IndexResponse index = restHighLevelClient.index(indexRequest, ElasticSearchConfig.COMMON_OPTIONS);
         DocWriteResponse.Result result = index.getResult();
-        if(DocWriteResponse.Result.CREATED.equals(index.getResult())){
-            System.out.println("创建索引 插入文档完毕！！result"+"result="+result);
+        if (DocWriteResponse.Result.CREATED.equals(index.getResult())) {
+            System.out.println("创建索引 插入文档完毕！！result" + "result=" + result);
         }
     }
 
     /**
      * 获取指定索引下的 id 文档
+     *
      * @throws IOException
      */
     @Test
@@ -78,14 +89,15 @@ public class ElasticSearchTest {
         Map<String, Object> source = documentFields.getSource();
         System.out.println(source);
     }
+
     @Test
     public void test003() throws IOException {
         try {
-            if(restHighLevelClient.ping(ElasticSearchConfig.COMMON_OPTIONS)){
+            if (restHighLevelClient.ping(ElasticSearchConfig.COMMON_OPTIONS)) {
                 System.out.println("链接成功es");
             }
-        }catch (Exception e){
-            if(e instanceof ElasticsearchException){
+        } catch (Exception e) {
+            if (e instanceof ElasticsearchException) {
                 System.out.println("ConnectException链接失败");
             }
         }
@@ -93,6 +105,7 @@ public class ElasticSearchTest {
 
     /**
      * 模糊查询
+     *
      * @throws IOException
      */
     @Test
@@ -118,6 +131,7 @@ public class ElasticSearchTest {
 
     /**
      * 高亮查询
+     *
      * @throws IOException
      */
     @Test
@@ -206,6 +220,279 @@ public class ElasticSearchTest {
         }
     }
 
+    @Test
+    public void createIndextest() {
+        Map<String, String> build = MapUtil.builder("asdf", "哈哈").build();
+        System.out.println(build);
+        String s = JSONUtil.toJsonStr(build);
+        System.out.println(s);
+    }
+
+
+    @Test
+    public void testTerm() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.termQuery("author", "胡八一"));
+        request.source(builder);
+
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testTerms() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.termsQuery("author", "胡八一", "鲁迅"));
+        request.source(builder);
+
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testMatchAll() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.matchAllQuery());
+        request.source(builder);
+
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testMatch() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.matchQuery("xnxx", "xnxx"));
+        request.source(builder);
+
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testBooleanMatch() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.matchQuery("descr", "讲述 了").operator(Operator.OR));
+//        builder.query(QueryBuilders.matchQuery("descr","讲述 了").operator(Operator.AND));
+        request.source(builder);
+
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testPrefix() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.prefixQuery("author", "胡"));
+        request.source(builder);
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testFuzzy() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.fuzzyQuery("descr", "讲述"));
+        request.source(builder);
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testRange() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.rangeQuery("count").gte(10).lt(2000));
+        request.source(builder);
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testRegexp() {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(10);
+        builder.query(QueryBuilders.regexpQuery("phone", "180[0-9]{8}"));
+        request.source(builder);
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
+
+    @Test
+    public void testScroll() throws IOException {
+        SearchRequest request = new SearchRequest("book");
+        request.scroll(TimeValue.MINUS_ONE);
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.size(2);
+        builder.sort("count", SortOrder.DESC);
+        builder.query(QueryBuilders.matchAllQuery());
+        request.source(builder);
+        SearchResponse search = null;
+        try {
+            search = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String scrollId = search.getScrollId();
+        System.out.println("-----------");
+        System.out.println(scrollId);
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+        //二次查询
+        while (true) {
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            scrollRequest.scroll(TimeValue.MINUS_ONE);
+            SearchResponse scrollResponse = restHighLevelClient.scroll(scrollRequest, RequestOptions.DEFAULT);
+            if (scrollResponse.getHits().getHits() != null && scrollResponse.getHits().getHits().length > 0) {
+                System.out.println("-----下一页------");
+                for (SearchHit hit : scrollResponse.getHits().getHits()) {
+                    System.out.println(hit.getSourceAsMap());
+                }
+            } else {
+                System.out.println("-----结束------");
+                break;
+            }
+        }
+        //删除scrollId
+        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        clearScrollRequest.addScrollId(scrollId);
+        ClearScrollResponse clearScrollResponse = restHighLevelClient.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+        System.out.println(clearScrollResponse.isSucceeded());
+    }
+
+
+
+
+    @Test
+    public void testdeleteByQuery() throws IOException {
+        DeleteByQueryRequest request = new DeleteByQueryRequest("book");
+        request.setQuery(QueryBuilders.regexpQuery("phone", "180[0-9]{8}"));
+        BulkByScrollResponse response = restHighLevelClient.deleteByQuery(request, RequestOptions.DEFAULT);
+        System.out.println(response.getDeleted());
+    }
+
+
+
+
+    //复合查询--布尔查询
+    @Test
+    public void testBool() throws IOException {
+        SearchRequest request = new SearchRequest("book");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.should(QueryBuilders.termQuery("phone","18011111111"));
+        boolQueryBuilder.should(QueryBuilders.termQuery("phone","18022222222"));
+        boolQueryBuilder.mustNot(QueryBuilders.termQuery("name","asdfasd"));
+        boolQueryBuilder.must(QueryBuilders.termQuery("count","20"));
+        builder.query(boolQueryBuilder);
+        request.source(builder);
+        SearchResponse search  = restHighLevelClient.search(request, ElasticSearchConfig.COMMON_OPTIONS);
+        for (SearchHit hit : search.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+
 
 
 }
+
+
+
