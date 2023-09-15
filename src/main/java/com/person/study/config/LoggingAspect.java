735a8231aad159e5e4a90459ce9913da3dbc2237
17600267646 <17600267646@163.com>
2023-09-15 15:22:45 +0800

    用切面实现调用方法时，打印入参以及返回结果

--- src/main/java/com/person/study/config/LoggingAspect.java
+++ src/main/java/com/person/study/config/LoggingAspect.java
@@ -1,6 +1,6 @@
 package com.person.study.config;
 
-import lombok.extern.slf4j.Slf4j;
+import cn.hutool.json.JSONUtil;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,34 +10,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
 
 @Aspect
 @Component
-@Slf4j
-public class LogAspect {
+public class LoggingAspect {
 
-//    private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("execution(public * com.person.study.*.*(..))") // 定义切点为com.example包下的所有方法
+    @Pointcut("execution(* com.person.study.controller..*(..))") // 定义切点为com.example包及其子包下的所有方法
     public void logPointcut() {}
 
     @Before("logPointcut()") // 在切点方法执行前执行
-    public void doBefore(JoinPoint joinPoint) {
+    public void logMethodEntry(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getTarget().getClass().getSimpleName();
-        String methodPath = className + "." + methodName;
-        log.info("Method Path: " + methodPath);
-        log.info("Arguments: " + Arrays.toString(joinPoint.getArgs()));
+        String methodPath = className + "/" + methodName;
+        logger.info("开始调用/" + methodPath+"请求参数"+ JSONUtil.toJsonStr(joinPoint.getArgs()));
+
     }
 
     @AfterReturning(pointcut = "logPointcut()", returning = "result") // 在切点方法执行后执行
-    public void doAfterReturning(JoinPoint joinPoint, Object result) {
+    public void logMethodExit(JoinPoint joinPoint, Object result) {
         String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getTarget().getClass().getSimpleName();
-        String methodPath = className + "." + methodName;
-        log.info("Method Path: " + methodPath);
-        log.info("Return: " + result);
+//        String methodPath = className + "/" + methodName;
+//        logger.error("Method Exit: " + methodPath);
+        logger.info("返回结果" + JSONUtil.toJsonStr(result));
     }
 }
